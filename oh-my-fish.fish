7e626d7eb35d4079d7db9caeefd85fb437b97408
Adam Horak <ahorak@frontlinetechnologies.com>
2019-02-05 08:33:42 -0800

    omf stuffs

---
+++ oh-my-fish.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/env fish
+
+curl -L https://get.oh-my.fish | fish
+
+# plugins
+omf install z
--- vs-code-ext.sh
+++ vs-code-ext.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 code --install-extension k--kato.intellij-idea-keybindings
+code --install-extension skyapps.fish-vscode
